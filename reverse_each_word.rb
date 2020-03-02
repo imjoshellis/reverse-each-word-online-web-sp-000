97bc69caa88a0cecc67b20443fa0970763321328
Josh Ellis <joshe523@gmail.com>
2020-03-02 23:06:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,8 @@ require 'pry'
 def reverse_each_word(sentence)
   arr = sentence.split(' ')
   binding.pry
-  # arr.collect do |word|
-  #   word.reverse
-  # end
-  # return arr.join(' ')
+  rev_arr = arr.collect do |word|
+    word.reverse
+  end
+  return rev_arr.join(' ')
 end
